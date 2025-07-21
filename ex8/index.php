295090b9c7f0e0bfa3fd13a4cbd7c658f6fe23cf
miguelcalisto <miguelcalistors@gmail.com>
2025-07-21 19:01:34 -0300

    fim da logica do ex 8

--- ex8/index.php
+++ ex8/index.php
@@ -9,14 +9,17 @@
     <?php 
     $num = $_GET["num"];
     $sqr = sqrt($num);
+    $sqr3 = pow($num , 1/3);
     ?>
     <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="get">
 
     <label for="TxtNum"> numero: </label>
-    <input type="number" id="TxtNum" value="<?php echo $num?>">
+    <input type="number" name="num" id="TxtNum" value="<?php echo $num?>">
     <input type="submit" value="enviar">
     </form>
-    <p>raiz quadrada = </p>
+    <p>raiz quadrada = <?php echo number_format($sqr , 2,"," , ".")?></p>
+    
+    <p>raiz cubica = <?php echo number_format($sqr3 , 2 , "," , ".")?></p>
 
 </body>
 </html>
